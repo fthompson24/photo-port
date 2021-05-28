0d7bc19bd47eea7c1ac526033f1e6265ca5d58e6
fthompson24 <fraserjthompson@gmail.com>
2021-05-28 16:45:05 -0500

    conditionally render pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import About from './components/About/index';
 import Nav from './components/Nav/index';
+import Gallery from './components/Gallery/index';
 
 function App() {
+  const [categories] = useState([
+    { name: "commercial", description: "Photos of grocery stores, food trucks, and other commercial properties" },
+    { name: "portraits", description: "Portaits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    { name: "landscape", description: "Fields, farmhouses, waterfalls, and the beauty of nature" },
+  ]);
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
   return (
     <div>
-      <Nav /> 
+      <Nav
+      categories={categories}
+      setCurrentCategory={setCurrentCategory}
+      currentCategory={currentCategory}
+      /> 
       <main>
+        <Gallery />
         <About />
       </main>    
     </div>
